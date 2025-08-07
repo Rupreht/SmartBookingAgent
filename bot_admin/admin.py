78e93e17cd8177d82413ea636abd5ef41ae01dfe
Nikolay Ermolovich <rupreht@rupreht.ru>
2025-08-07 10:11:07 +0500

    upd: pylintrc

--- bot_admin/admin.py
+++ bot_admin/admin.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from .models import TelegramUser, TelegramUserProfilePhotos
+# from django.contrib import admin
+# from .models import TelegramUser, TelegramUserProfilePhotos
 
 
-@admin.register(TelegramUser, TelegramUserProfilePhotos)
-class PersonAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(TelegramUser, TelegramUserProfilePhotos)
+# class PersonAdmin(admin.ModelAdmin):
+#     pass
--- bot_admin/apps.py
+++ bot_admin/apps.py
@@ -1,6 +1,10 @@
+"""Main class Bot Admin"""
+
 from django.apps import AppConfig
 
 
 class SbaAdminConfig(AppConfig):
+    """SbaAdminConfig"""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "bot_admin"
